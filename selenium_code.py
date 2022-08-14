6b1e4571e98f1ba2edc90fce3b36233110878f12
connorpobrien <54494032+connorpobrien@users.noreply.github.com>
2022-08-14 10:30:10 -0700

    Creation of project

--- selenium_code.py
+++ selenium_code.py
@@ -1,6 +1,7 @@
 
 import time
-from selenium import webdriver
+import webbrowser
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -9,7 +10,7 @@ from selenium.webdriver.chrome.service import Service
 s = Service('/usr/local/bin/chromedriver')
 
 
-def refresh_page(page, seconds):
+def refresh_page(page, seconds, xpath):
     """
     :param page: link to site -> str
     :param seconds: how often to reload page -> int
@@ -17,16 +18,41 @@ def refresh_page(page, seconds):
     """
     driver = webdriver.Chrome(service=s)
     driver.get(page)
-    time.sleep(seconds)
 
-    driver.refresh()
+    # infinite loop to reload page and search for
+    num_reloads = 1
+    while num_reloads > 0:
+        time.sleep(seconds)
+        driver.refresh()
+        driver.find_element(By.XPATH, xpath).click()
+
+def search_page(xpath):
+    """
+    :param xpath: xpath to search for on a page
+    :return: True if phrase is found, false if not
+    """
+    return webbrowser.find_element_by_xpath(xpath)
+
+
+def notify(email):
+    """
+    :param email: email address of recipient
+    :return: sends email to address if search_page is found to be true
+    """
+    pass
 
 
 def main():
-    webpage = input("Enter the url for the webpage: ")
-    seconds = input("Enter how often you would like to reload the page: ")
-    refresh_page(webpage, seconds)
+    # user input data
+    webpage_url = input("Enter the url for the webpage: ")
+    seconds = eval(input("Enter how often you would like to reload the page (seconds): "))
+    content_xpath = input("Enter what you are searching for on the page: ")
+    email = input("Enter your email: ")
+    print("If '{0}' is found on '{1}', you will receive an email at '{2}'!".format(content_xpath, webpage_url, email))
+
+    # run code
+    refresh_page(webpage_url, seconds, content_xpath)
 
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     main()
