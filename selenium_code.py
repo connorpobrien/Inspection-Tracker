94026859b7026488f7c3b9031834e86f7e90edec
connorpobrien <54494032+connorpobrien@users.noreply.github.com>
2022-08-14 09:39:22 -0700

    Creation of project

--- selenium_code.py
+++ selenium_code.py
@@ -8,8 +8,25 @@ from selenium.webdriver.chrome.service import Service
 # set location to the location of the webdriver
 s = Service('/usr/local/bin/chromedriver')
 
-driver = webdriver.Chrome(service=s)
-driver.get("https://amazon.com/")
-time.sleep(5)
 
-driver.refresh()
+def refresh_page(page, seconds):
+    """
+    :param page: link to site -> str
+    :param seconds: how often to reload page -> int
+    :return: uses selenium to reload designated chrome page
+    """
+    driver = webdriver.Chrome(service=s)
+    driver.get(page)
+    time.sleep(seconds)
+
+    driver.refresh()
+
+
+def main():
+    webpage = input("Enter the url for the webpage: ")
+    seconds = input("Enter how often you would like to reload the page: ")
+    refresh_page(webpage, seconds)
+
+
+if __name__ == '__main__'
+    main()
